1af5a0326bd20d6855f68cb166a664bde4f372dd
3omar63 <om728526@gmail.com>
2025-01-17 11:04:35 +0200

    solved isuue of oldest age

--- Session01/Program.cs
+++ Session01/Program.cs
@@ -31,7 +31,7 @@ namespace Session01
             //            }
 
             //        } while (!(flag1&&flag2));
-                    
+
             //        points[i] = new Point()
             //        {
             //            X=x,
@@ -54,40 +54,60 @@ namespace Session01
 
             #region 2	Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
 
-//            Person[] persons = new Person[3];
+            Person[] persons = new Person[3];
 
-//            for (int i = 0; i < persons.Length; i++)
-//            {
-//                try
-//                {
-//                    bool flag;
-//                    int age;
-//                    Console.WriteLine($"Enter details for person {i + 1}:");
+            for (int i = 0; i < persons.Length; i++)
+            {
+                try
+                {
+                    bool flag;
+                    int age;
+                    string name;
+                    Console.WriteLine($"Enter details for person {i + 1}:");
 
-//                    do
-//                    {
-//                        Console.Write("Age: ");
-//                        flag = int.TryParse(Console.ReadLine(), out  age);
-//                        if (!flag)
-//                           Console.WriteLine("Invalid input. Please enter valid integers for Age.");
-//                    } while (!flag);
-//                    Console.Write("Name: ");
-//                    string name = Console.ReadLine();
+                    do
+                    {
+                        Console.Write("Name: ");
+                        name = Console.ReadLine();
 
-//                    persons[i] = new Person()
-//                    {
-//                        Age = age,
-//                        Name = name
-//                    };
-//                }
-//                catch (Exception ex) {
+                    }
+                    while (string.IsNullOrEmpty(name));
 
-//                    Console.WriteLine(ex.Message);
-//                }
-//}
-//            Console.WriteLine($"Person 1: Name = {persons[0].Name}, Age = {persons[0].Age}");
+                    do
+                    {
+                        Console.Write("Age: ");
+                        flag = int.TryParse(Console.ReadLine(), out age);
+                        if (!flag)
+                            Console.WriteLine("Invalid input. Please enter valid integers for Age.");
+                    } while (!flag);
+                   
 
-            #endregion
+                    persons[i] = new Person()
+                    {
+                        Age = age,
+                        Name = name
+                    };
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            int oldestIndex = 0; 
+            for (int i = 1; i < persons.Length; i++)
+            {
+                if (persons[i].Age > persons[oldestIndex].Age)
+                {
+                    oldestIndex = i; 
+                }
+            }
+            Console.WriteLine($"Oldest Person: Name = {persons[oldestIndex].Name}, Age = {persons[oldestIndex].Age}");
+
         }
+
+        #endregion
     }
-}
+    }
+
