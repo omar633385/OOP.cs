6c2b1658a9789caa7acb1cc4e8abe4e3f03af094
3omar63 <om728526@gmail.com>
2025-01-19 13:18:49 +0200

    part 1 q4

---
+++ Session04/Part 1/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session04.Part_1
+{
+    internal class Manager:Employee
+    {
+        //b) Create a derived class named Manager that overrides the Work method to print "Manager is managing". 
+        public override void Print()
+        {
+            Console.WriteLine("Manager is managing");
+        }
+    }
+}
