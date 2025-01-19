716dab1ab9e7aed37cf82789a6438bee96b7fff9
3omar63 <om728526@gmail.com>
2025-01-19 12:57:23 +0200

    finished part 1 q2

---
+++ Session04/Part 1/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session04.Part_1
+{
+    #region Part1 Q2
+    ///    Q2) Create a class named Rectangle with the following constructors:
+    ///●	A parameterless constructor that sets the width and height to 0.
+    ///●	A constructor that accepts width and height as integers.
+    ///●	A constructor that accepts a single integer and sets both width and height to that value
+
+    internal class Rectangle
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public Rectangle()
+        {
+        }
+        public Rectangle(int value)
+        {
+            Width = value;
+            Height = value;
+        }
+    } 
+    #endregion
+
+
+}
