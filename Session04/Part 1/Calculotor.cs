6e85f717703e0d1c662b4510c2b4fe0e9e5c080c
3omar63 <om728526@gmail.com>
2025-01-19 12:52:40 +0200

    finished part 1 q1

---
+++ Session04/Part 1/Calculotor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session04
+{
+    internal class Calculotor
+    {
+        #region Part01Q1
+        ///       Q1) Write a class named Calculator that contains a method named Add.Overload the Add method to:
+        ///Accept two integers and return their sum.
+        ///   Accept three integers and return their sum.
+        ///   Accept two doubles and return their sum.
+        ///   
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Add(int x, int y, int z)
+        {
+
+            return x + y + z;
+        }
+        public double Add(double x, double y)
+        {
+
+            return x + y;
+        } 
+        #endregion
+    }
+}
