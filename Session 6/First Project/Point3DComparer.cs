9ee08adeb0ebcb21b50071bea35ba380182421d1
3omar63 <om728526@gmail.com>
2025-01-26 23:48:21 +0200

    Finished First Project

---
+++ Session 6/First Project/Point3DComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Session_6.First_Project
+{
+    //  5.	Define an array of points and sort this array based on X & Y coordinates.
+    internal class Point3DComparer: IComparer
+    {
+        public int Compare(object? x, object? y)
+        {
+            Point3D p1 = (Point3D)x;
+            Point3D p2 = (Point3D)y;
+            int compareX = p1.X.CompareTo(p2.X);
+            if (compareX != 0)
+                return compareX;
+
+            // If X coordinates are equal, compare Y coordinates
+            return p1.Y.CompareTo(p2.Y);
+
+        }
+    }
+}
---
+++ Session 6/Program.cs
@@ -0,0 +1,105 @@
+﻿using Session_6.First_Project;
+
+namespace Session_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First Project
+
+            //Point3D P = new Point3D(10, 10, 10);
+            //Console.WriteLine(P.ToString());
+
+
+            #region 3.	Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+            //Point3D[] points = new Point3D[2];
+            //for (int i = 0; i < points.Length; i++)
+            //{
+            //    try
+            //    {
+            //        int x, y, z;
+            //        bool flag;
+            //        Console.WriteLine($"Enter details for point {i + 1}:");
+
+            //        do
+            //        {
+            //            Console.Write("x: ");
+            //            flag = int.TryParse(Console.ReadLine(), out x);
+            //            Console.Write("y: ");
+            //            flag = int.TryParse(Console.ReadLine(), out y);
+            //            Console.Write("z: ");
+            //            flag = int.TryParse(Console.ReadLine(), out z);
+
+            //            if (!flag)
+            //            {
+            //                Console.WriteLine("Invalid input. Please enter valid integers for x,y and z.");
+            //            }
+            //            points[i] = new Point3D()
+            //            {
+            //                X = x,
+            //                Y = y,
+            //                Z = z
+            //            };
+
+            //        } while (!flag);
+            //    }
+            //    catch (Exception ex)
+            //    {
+
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
+
+            #endregion
+
+
+            #region 4.	Try to use  ==If(P1 == P2)   Does it work properly? 
+            //Console.WriteLine($"P1 hashcode:{points[0].GetHashCode()}");
+            //Console.WriteLine($"P2 hashcode:{points[1].GetHashCode()}");
+
+            //if (points[0] == points[1])
+            //{
+            //    Console.WriteLine("They Are Equal");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("They are not Equal");
+            //}
+            //It Works As they are two different Objects Event if they have the same state 
+            #endregion
+
+
+
+            #region 5.	Define an array of points and sort this array based on X & Y coordinates.
+
+            //Point3D[] points3ds =
+            //    {
+            //        new Point3D { X = 3, Y = 5, Z = 1 },
+            //        new Point3D { X = 1, Y = 2, Z = 3 },
+            //        new Point3D { X = 3, Y = 4, Z = 2 },
+            //        new Point3D { X = 1, Y = 1, Z = 4 }
+
+            //    };
+            //Array.Sort(points3ds, new Point3DComparer());
+
+            //Console.WriteLine("After Sorting");
+            //foreach (var item in points3ds)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+
+            #endregion
+
+            #region SecondProject
+
+
+            #endregion
+
+
+        }
+    }
+}
